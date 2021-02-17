228bdbd9d70626aca9caf49f80f1325f795f76c0
RicsonSampaio <ricsonsampaio@gmail.com>
2021-02-17 18:58:33 -0300

    Add-Automapper

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAllCamps()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+                var mapperResult = _mapper.Map<IEnumerable<CampModel>>(result);
+                return Ok(mapperResult);
+            }
+            catch (Exception)
+            {
+                // TODO logging
+                return InternalServerError();
+            }
+            
+        } 
+
+    }
+}
---
+++ starting/TheCodeCamp/Data/CampingMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampingMappingProfile : Profile
+    {
+        public CampingMappingProfile()
+        {
+            CreateMap<Camp, CampModel>();
+        }
+    }
+}
